f016dca3af87625a7c302a751c1678ae97a5aa42
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-09 21:27:05 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :welcome
+
+  get '/auth/facebook/callback' => 'sessions#create'
+
 end
